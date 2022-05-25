0259fc021e057755bb2c661eea31e5c8d32cddf3
sankalesh8668229890 <sankalesh.harak1998@gmail.com>
2022-05-25 15:15:00 +0530

    4api done

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -4,6 +4,8 @@ const jwt = require("jsonwebtoken");
 const aws = require("../aws/aws")
 const bcrypt = require("bcrypt")
 
+/*************************************************creting user*************************************************************************** */
+
 let createUser = async (req,res) =>{
     try {
         // extract data and file from RequestBody
@@ -82,6 +84,8 @@ let createUser = async (req,res) =>{
         res.status(500).send({status:false, message:error.message})
     }
 }
+/********************************************************************************************************************************************** */
+
 
 const loginUser = async function (req, res) {
   try{
@@ -111,7 +115,7 @@ const loginUser = async function (req, res) {
        {
         userId: findUser._id.toString(),
         iat: currTime,
-        exp: currTime + 1200
+        exp: currTime + 84600
       }, "functionUp"
 
     );
@@ -138,24 +142,150 @@ const getDetails = async function (req, res) {
           }  
           if (userId != req.userId) {  
             return res.status(401).send({ status: false, message: "not Authorize" });
-        } 
-              const finalData = {
-                  address: findData.address,
-                  _id: findData._id,
-                  fname: findData.fname,
-                  lname: findData.lname,
-                  email: findData.email,
-                  profileImage: findData.profileImage,
-                  phone: findData.phone,
-                  password: findData.password,
-              }
-              return res.status(200).send({ status: true, message: "User Profile Details...", data: finalData })
+          } 
+            return res.status(200).send({ status: true, message: "User Profile Details...", data: findData })
 
   } catch (err) {
       console.log(err);
       res.status(500).send({ status: false, message: err });
   }
 }
+/***************************************************update code******************************************************************************* */
 
-module.exports = {createUser, loginUser, getDetails}
+const updateUser = async function(req, res) {
+
+  let data = req.body;
+  const files = req.files
+  
+  const userIdFromParams = req.params.userId
+  const userIdFromToken = req.userId
+
+  const { fname, lname, email, phone, password } = data
+
+  const updatedData = {}
+
+  if (!validator.isValidObjectId(userIdFromParams)) { 
+    return res.status(400).send({ status: false, message: "Valid userId is required" })
+   }
+
+   const userByuserId = await userModel.findById(userIdFromParams);
+
+        if (!userByuserId) {
+            return res.status(404).send({ status: false, message: 'user not found.' });
+        }
+        if (userIdFromToken != userIdFromParams) {
+            return res.status(403).send({status: false,message: "Unauthorized access."});//check
+        }
+        if (Object.keys(data) == 0) {
+            return res.status(400).send({status: false,message: "please provide data to update"})
+        }
+  //=======================================fname validation=====================================
+  if (fname) {
+      if (!(validator.isValid(fname))) {
+          return res.status(400).send({ status: false, Message: "First name is required" })
+      }
+      updatedData.fname = fname
+  }
+  //===================================lname validation==========================================
+  if (lname) {
+      if (!(validator.isValid(lname))) {
+          return res.status(400).send({ status: false, Message: "Last name is required" })
+      }
+      updatedData.lname = lname
+  }
+  //================================email validation==============================================
+  if (email) {
+        
+        if (!(validator.isValid(email))) { return res.status(400).send({status:false, message:"please enter email"}) }
+        if (!(validator.isEmailValid(email))) { return res.status(400).send({status:false, message:"please enter valid Email"}) }
+      }
+      const isEmailUsed = await userModel.findOne({ email: email })
+      if (isEmailUsed) {
+          return res.status(400).send({ status: false, message: "email is alredy exist" })
+      }
+      updatedData.email = email
+  
+  //=======================profile pic upload and validation==========================
+
+  
+  if ( files && files.length > 0){
+      let uploadFileUrl = await aws.uploadFile(files[0])
+      updatedData.profileImage = uploadFileUrl
+}
+  //===============================phone validation-========================================
+
+  if (phone) {
+      if (!(validator.isValid(phone))) { return res.status(400).send({status:false, message:"please enter phone no."}) }
+
+      if (!(validator.isPhoneValid(phone))) { return res.status(400).send({status:false, message:"please enter valid phone"}) }
+      updatedData.phone = phone
+
+      const duplicatePhone = await userModel.findOne({phone:phone});
+        if(duplicatePhone) {return res.status(400).send({status:false, message:"phone is already exist"})};
+  }
+  //======================================password validation-====================================
+  if (password) {
+      if (!password) return res.status(400).send({ status: false, message: "please enter password"})
+
+      if(password.trim().length<8 || password.trim().length>15) {return res.status(400).send({ status: false, message: 'Password should be of minimum 8 characters & maximum 15 characters' })}
+
+    // using bcrypt
+    const rounds = 10;
+     let hash = await bcrypt.hash(password, rounds);
+     
+      updatedData.password = hash
+  }
+  //========================================address validation=================================
+
+  let address = JSON.parse(data.address)
+    data.address = address
+
+    if (!(validator.isValid(address.shipping.street)))
+         { return res.status(400).send({ status: true, message: " Street address is required" }) }
+
+          updatedData["address.shipping.street"] = address.shipping.street
+
+          if (!(validator.isValid(address.shipping.city))) 
+          { return res.status(400).send({ status: true, message: " city address is required" }) }
+
+          updatedData["address.shipping.city"] = address.shipping.city
+          
+          if (!(validator.isValid(address.shipping.pincode))) { return res.status(400).send({ status: true, message: " pincode address is required" }) }
+
+          if (!(validator.isValidPincode(address.shipping.pincode))) { return res.status(400).send({status:false, message:"Please provide pincode in 6 digit number"})}
+          updatedData["address.shipping.pincode"] = address.shipping.pincode
+      
+
+      
+          if (!(validator.isValid(address.billing.street)))
+        { return res.status(400).send({ status: true, message: " Street address is required" }) }
+
+          updatedData["address.billing.street"] = address.billing.street
+
+          if (!(validator.isValid(address.billing.city))) 
+          { return res.status(400).send({ status: true, message: " city address is required" }) }
+
+          updatedData["address.billing.city"] = address.billing.city
+
+          if (!(validator.isValid(address.billing.pincode))) { return res.status(400).send({ status: true, message: " pincode address is required" }) }
+
+        if (!(validator.isValidPincode(address.billing.pincode))) { return res.status(400).send({status:false, message:"Please provide pincode in 6 digit number"})}
+          updatedData["address.billing.pincode"] = address.billing.pincode
+      
+  
+
+  
+      
+          
+  
+
+  //=========================================update data=============================
+
+  const updatedUser = await userModel.findOneAndUpdate({ _id: userIdFromParams }, updatedData, { new: true })
+
+  return res.status(200).send({ status: true, message: "User profile updated", data: updatedUser });
+
+}
+
+module.exports = {createUser, loginUser, getDetails,updateUser}
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,7 +6,7 @@ const middleWare = require("../middleWare/auth")
 router.post("/register",userController.createUser)
 router.post("/login",userController.loginUser)
 router.get("/user/:userId/profile", middleWare.userAuth, userController.getDetails)
-
+router.put("/user/:userId/profile", middleWare.userAuth, userController.updateUser)
 
 
 module.exports = router;
